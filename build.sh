7700c8f1e14381dbb4fbc4cc94015ebfd84391a9
Lukas Gentele <lg@devspace.cloud>
2020-04-07 17:15:12 +0200

    improve build

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-mvn clean package -U -Dmaven.test.skip=true -Djar.finalName=main
+# Build jar file
+mvn clean package -U -Dmaven.test.skip=true
 
+# Rename jar file
+mv target/*.jar target/main.jar
+
+# Start application if `run` argument is passed
 if [ $1 = "run" ]; then
   java -jar target/main.jar
 fi
