942690d1584c8a876b193cd44005908e6ba2f2af
Fabian Kramm <fab.kramm@googlemail.com>
2021-09-20 13:48:35 +0200

    Update devspace_start.sh

--- devspace_start.sh
+++ devspace_start.sh
@@ -20,7 +20,7 @@ Welcome to your development container!
 This is how you can work with it
 - Run \`${COLOR_CYAN}./build.sh run${COLOR_RESET}\` to run the application
 - ${COLOR_CYAN}Files will be synchronized${COLOR_RESET} between your local machine and this container
-- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}localhost${COLOR_RESET}:3000
+- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}localhost${COLOR_RESET}:8080
 "
 
 bash
