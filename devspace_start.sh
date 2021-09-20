36bdedce07f307a31d11f73b0be6bb8d1f8989c9
Fabian Kramm <fab.kramm@googlemail.com>
2021-09-20 11:50:11 +0200

    New file

---
+++ devspace_start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set +e  # Continue on errors
+
+echo "Installing maven dependencies"
+mvn -T 1C install
+
+COLOR_CYAN="\033[0;36m"
+COLOR_RESET="\033[0m"
+
+echo -e "${COLOR_CYAN}
+   ____              ____
+  |  _ \  _____   __/ ___| _ __   __ _  ___ ___
+  | | | |/ _ \ \ / /\___ \| '_ \ / _\` |/ __/ _ \\
+  | |_| |  __/\ V /  ___) | |_) | (_| | (_|  __/
+  |____/ \___| \_/  |____/| .__/ \__,_|\___\___|
+                          |_|
+${COLOR_RESET}
+Welcome to your development container!
+
+This is how you can work with it
+- Run \`${COLOR_CYAN}./build.sh run${COLOR_RESET}\` to run the application
+- ${COLOR_CYAN}Files will be synchronized${COLOR_RESET} between your local machine and this container
+- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}localhost${COLOR_RESET}:3000
+"
+
+bash
